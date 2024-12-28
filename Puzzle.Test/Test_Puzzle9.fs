4d9c68705aaf8d5d4970d70624e423eb081b40fa
Tom Liu <TLiu@nzsuperfund.co.nz>
2024-12-29 07:09:13 +1300

    day 9 not pass

--- Puzzle.Test/Test_Puzzle9.fs
+++ Puzzle.Test/Test_Puzzle9.fs
@@ -27,4 +27,24 @@ let ``Test Populate File Layout`` () =
     let map = parseInput "../../../../inputs/test_day9.txt"
     let fileLayout = populateFileLayout map
 
-    Assert.Equal("00...111...2...333.44.5555.6666.777.888899", fileLayout)
+    Assert.Equal("00...111...2...333.44.5555.6666.777.888899", fileLayout |> String.concat "")
+
+[<Fact>]
+let ``Test Process File`` () =
+    let map = parseInput "../../../../inputs/test_day9.txt"
+    let fileLayout = populateFileLayout map
+    let processedFile = processFile fileLayout
+
+    Assert.Equal("0099811188827773336446555566..............", processedFile |> String.concat "")
+
+[<Fact>]
+let ``Test calculateChecksum`` () =
+    let map = parseInput "../../../../inputs/day9.txt"
+    let fileLayout = populateFileLayout map
+    let processedFile = processFile fileLayout
+    let checksum = calculateChecksum processedFile
+
+    Assert.Equal(89425419840L, checksum)
+
+
+
--- puzzles/puzzle9.fs
+++ puzzles/puzzle9.fs
@@ -4,9 +4,9 @@ open System
 
 module puzzle9 =
     type DiskMap = string
-    type FileLayout = string
+    type FileLayout = string array
 
-    let parseInput (filePath: string) : string = System.IO.File.ReadAllText(filePath)
+    let parseInput (filePath: string) : DiskMap = System.IO.File.ReadAllText(filePath)
 
     let populateGapBlock (n: int) : string = String.replicate n "."
 
@@ -15,35 +15,34 @@ module puzzle9 =
     let populateFileLayout (map: DiskMap) : FileLayout =
 
         map
-        |> Seq.mapi (fun i c ->
-            let n = int (System.Char.GetNumericValue(c))
+        |> Seq.mapi (fun (i: int) (c: char) ->
+            let n = System.Int32.Parse(string c)
             if i % 2 = 0 then
                 populateFileBlock (n, i / 2)
             else
                 populateGapBlock (n))
+        |> Seq.toArray
 
-        |> String.concat ""
-
-    let processFile (layoutS: FileLayout) : FileLayout =
-        let layout = layoutS.ToCharArray()
+    let processFile (layout: FileLayout) : FileLayout =
         let mutable i = 0
         let mutable j = layout.Length - 1
 
         while i < j do
-            if layout.[i] = '.' && layout.[j] <> '.' then
+            if layout.[i] = "." && layout.[j] <> "." then
                 layout.[i] <- layout.[j]
-                layout.[j] <- '.'
+                layout.[j] <- "."
                 i <- i + 1
                 j <- j - 1
-            else if layout.[i] = '.' && layout.[j] = '.' then
+            else if layout.[i] = "." && layout.[j] = "." then
                 j <- j - 1
-            else if layout.[i] <> '.' && layout.[j] = '.' then
+            else if layout.[i] <> "." && layout.[j] = "." then
                 i <- i + 1
                 j <- j - 1
             else
                 i <- i + 1
 
-        layout |> FileLayout
+        layout
 
-    let calculateChecksum (layout: FileLayout) : int =
-        layout |> Seq.mapi (fun i c -> if c <> '.' then i * int c else 0) |> Seq.sum
+    let calculateChecksum (layout: FileLayout)  =
+        layout |> Seq.mapi (fun i c -> if c <> "." then int64 i * int64(System.Int32.Parse(c)) else 0) |> Seq.sum
+
